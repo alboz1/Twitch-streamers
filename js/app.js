ac0f8788b153f08077f18f0b0fece7fb6e3a216e
alboz1 <vucajjuerd@gmail.com>
2017-06-28 03:09:16 +0200

    added feature to detect if an account doesnt exist

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const app = {
-	streamers: ['freecodecamp','grossie_gore','tsm_bjergsen','Tsm_Dyrus','summit1g','pobelter'],
+	streamers: ['freecodecamp','grossie_gore','tsm_bjergsen','Tsm_Dyrus','summit1g','pobelter','asd'],
 	online: false,
 
 	init() {
-		$.each(this.streamers, index => {
-			this.onlineInfo(index);
+		this.streamers.forEach(streamer => {
+			this.onlineInfo(streamer);
 		});
 		this.eventListeners();
 	},
@@ -39,11 +39,10 @@ const app = {
 		});
 	},
 
-	onlineInfo(index) {
-		this.getData(`https://wind-bow.gomix.me/twitch-api/streams/${this.streamers[index]}`, data => {
-			//console.log(data);
+	onlineInfo(streamer) {
+		this.getData(`https://wind-bow.gomix.me/twitch-api/streams/${streamer}`, data => {
 			if (data.stream === null) {
-				this.offlineInfo(index);
+				this.offlineInfo(streamer);
 			} else {
 				this.online = true;
 				
@@ -59,37 +58,36 @@ const app = {
 		});
 	},
 
-	offlineInfo(index) {
-		this.getData(`https://wind-bow.gomix.me/twitch-api/channels/${this.streamers[index]}`, data => {
+	offlineInfo(streamer) {
+		this.getData(`https://wind-bow.gomix.me/twitch-api/channels/${streamer}`, data => {
 			this.online = false;
 
 			this.render({
 				status: 'Offline',
 				name: data.display_name,
 				logo: data.logo,
-				url: data.url
+				url: data.url,
+				error: data.status,
+				streamer: streamer
 			});
 		});
 	},
 
 	render(props) {
 		$('.wrapper').append(`
-			<div class="box" style="background: url('${props.logo}')">
-				<span class="status-${props.status}">${props.status}</span>
-				<p class="name">${props.name}</p>
-				<p class="game">${props.game}</p>
-				<p class="info">${props.info}</p>
+			<div class="box" ${props.error === 422 ? 'style="background: white"' : `style="background: url('${props.logo}')"`}>
+				<span class="status-${props.status}">
+					${props.error === 422 ? 'Account doesn\'t exist' : props.status}
+				</span>
+				<p class="name">${props.error === 422 ? props.streamer : props.name}</p>
+				${(props.error === 422 || !this.online) ? '' : `<p class="game">${props.game}</p>`}
+				${(props.error === 422 || !this.online) ? '' : `<p class="info">${props.info}</p>`}
 			</div>
 		`);
-		
-		if (this.online) {
-			$('.box .game').css('display', 'inline-block');
-			$('.box .info').css('display', 'inline-block');
-		}
 		
 		$('.box').on('click', function() {
 			window.open(props.url);
 		});
 	}
-};
+}
 app.init();
